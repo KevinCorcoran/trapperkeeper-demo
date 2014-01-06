650185aa9b40618719283b832654676a91719b40
Kevin Corcoran <kevin.corcoran@puppetlabs.com>
2014-01-06 12:42:46 -0800

    default logging is 2 seconds

--- src/puppetlabs/trapperkeeper_demo/core.clj
+++ src/puppetlabs/trapperkeeper_demo/core.clj
@@ -5,13 +5,13 @@
 (defservice hello-world-service
   {:depends []
    :provides []}
-  (log-periodically "Hello, world!" 5)
+  (log-periodically "Hello, world!" 2)
   {})
 
 (defservice animal-service
   {:depends [[:config-service get-in-config]]
    :provides []}
   (let [msg       (get-in-config [:animals :sound] "MOOOO, I am a cow.")
-        interval  (get-in-config [:animals :interval] 5)]
+        interval  (get-in-config [:animals :interval] 2)]
     (log-periodically msg interval))
   {})
